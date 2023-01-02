442c7091b906ff878b3c029f91c2f2dec3ee5187
karkimilan <Milankarki723@gmail.com>
2023-01-02 13:04:05 +0545

    Comments

--- Program.cs
+++ Program.cs
@@ -1,35 +1,19 @@
-﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
-
-string quantity = "";
-string output = "";
-
-// Your work here
-
-const string spanTag = "<span>";
-
-// Extract the quantity
-int quantityStart = input.IndexOf(spanTag);
-int quantityEnd = input.IndexOf("</span>");
-quantityStart += spanTag.Length;
-int quantityLength = quantityEnd - quantityStart;
-quantity = input.Substring(quantityStart, quantityLength);
-
-// Set output to input, replacing the trademark symbol with the registered trademark symbol
-output = input.Replace("&trade;", "&reg;");
-
-// Remove the opening <div> tag
-int divStart = input.IndexOf("<div");
-int divEnd = input.IndexOf(">");
-int divLength = divEnd - divStart;
-divLength += 1;
-output = output.Remove(divStart, divLength);
-
-// Remove the closing <div> tag
-int divCloseStart = output.IndexOf("</div");
-int divCloseEnd = output.IndexOf(">", divCloseStart);
-int divCloseLength = divCloseEnd - divCloseStart;
-divCloseLength += 1;
-output = output.Remove(divCloseStart, divCloseLength);
-
-Console.WriteLine($"Quantity: {quantity}");
-Console.WriteLine($"Output: {output}");
+﻿Random random = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, padd with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
+}
+// Print out each orderID
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
