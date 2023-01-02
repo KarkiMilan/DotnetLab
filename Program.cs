0ddfd85a2a27b97de07af28dcbe4a8a33b494f96
karkimilan <Milankarki723@gmail.com>
2023-01-02 10:29:00 +0545

    else if and else statements without code blocks

--- Program.cs
+++ Program.cs
@@ -1,27 +1,8 @@
-﻿using System;
+﻿string name = "steve";
 
-namespace MyNewApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string value = "Microsoft Learn";
-            string reversedValue = MyNewApp.Utilities.Utility.Reverse(value);
-            Console.WriteLine($"Secret message: {reversedValue}");
-        }
-    }
-}
-
-namespace MyNewApp.Utilities
-{
-    class Utility
-    {
-        public static string Reverse(string message)
-        {
-            char[] letters = message.ToCharArray();
-            Array.Reverse(letters);
-            return new string(letters);
-        }
-    }
-}
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve") 
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
