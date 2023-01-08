f9ee8e60772aef9ce5a57ea38d24d7aef62b3d58
Milan <milankarki723@gmail.com>
2023-01-08 18:49:21 +0545

    Use of class object fileds and methods

--- Program.cs
+++ Program.cs
@@ -1,18 +1,34 @@
-﻿Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
-    if ((roll1 == roll2) && (roll2 == roll3)) {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6; 
-    } else {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DemoApplication
+{
+ class Tutorial
+ {
+  int TutorialID; 
+  string TutorialName;
+  
+  public void SetTutorial(int pID,string pName) 
+  {
+   TutorialID=pID;
+   TutorialName=pName;
+  }
+  public String GetTutorial()
+  {
+   return TutorialName;
+  }
+  
+  static void Main(string[] args) 
+  {
+   Tutorial pTutor=new Tutorial();
+    
+   pTutor.SetTutorial(1,".Net");
+    
+   Console.WriteLine(pTutor.GetTutorial());
+    
+   Console.ReadKey(); 
+  }
+ }
 }
